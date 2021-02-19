b1510cbaa1ae137f4393e4800d63ff94e97f2183
Samassart <74546158+Samassart@users.noreply.github.com>
2021-02-19 15:48:41 +0100

    Update tilewarper.py
    Can read .TIFF file as well

--- tilewarper/tilewarper.py
+++ tilewarper/tilewarper.py
@@ -242,7 +242,7 @@ class TileWarper(object):
         filepaths = []
         if not os.path.isfile(filepath):
             for dir_path, _, _ in os.walk(filepath):
-                filepaths.extend(glob.glob(os.path.join(dir_path, '*.tif')))
+                filepaths.extend(glob.glob(os.path.join(dir_path, '*.tif*')))
         else:
             filepaths = [filepath]
         return filepaths
